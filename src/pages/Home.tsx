b3cf295b4c007759351c761d1e61576068b6f24e
Seeya0 <vinlandtorfin@gmail.com>
2023-02-12 00:01:03 +0900

    Home画面 一旦完成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
 import React, { useState } from 'react';
-import { FormField, Loader } from '../components';
+import { Card, FormField, Loader } from '../components';
+
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => (
+      <Card
+        key={post._id}
+        {...post}
+      />
+    ));
+  }
+
+  return <h2 className='mt-5 font-bold text-[#6449ff] text-xl uppercase'>{title}</h2>;
+};
 
 const Home = () => {
   const [loading, setLoading] = useState(false);
@@ -34,6 +47,19 @@ const Home = () => {
             )}
           </>
         )}
+        <div className='grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
+          {searchText ? (
+            <RenderCards
+              data={[]}
+              title='No search results found'
+            />
+          ) : (
+            <RenderCards
+              data={[]}
+              title='No Posts found'
+            />
+          )}
+        </div>
       </div>
     </section>
   );
